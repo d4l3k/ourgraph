68cb4890f3e893f8af1c20603814e0c9a9770f89
Tristan Rice <rice@fn.lc>
2019-08-28 21:26:21 -0700

    db: embed CA instead of providing as env variable

--- db/db.go
+++ db/db.go
@@ -13,20 +13,40 @@ import (
 
 var dgraphAddr = flag.String("dgraphaddr", "ourgraph-db.fn.lc:9080", "address of the dgraph instance")
 
+const CA = `-----BEGIN CERTIFICATE-----
+MIIDNDCCAhygAwIBAgIIckKrr7Q0fT8wDQYJKoZIhvcNAQELBQAwRjEaMBgGA1UE
+ChMRRGdyYXBoIExhYnMsIEluYy4xFzAVBgNVBAMTDkRncmFwaCBSb290IENBMQ8w
+DQYDVQQFEwY3MjQyYWIwHhcNMTkwODIyMDM1ODMxWhcNMjkwODIxMDM1ODMxWjBG
+MRowGAYDVQQKExFEZ3JhcGggTGFicywgSW5jLjEXMBUGA1UEAxMORGdyYXBoIFJv
+b3QgQ0ExDzANBgNVBAUTBjcyNDJhYjCCASIwDQYJKoZIhvcNAQEBBQADggEPADCC
+AQoCggEBANu3jXmET5D0dHTa/OZHxxKzg203GqZ7lETXudZdTuZdJgl9yTRerDu6
+YnfbDIC4fiAya5kYtb1zoRtsrQIRBa0y4jbHSBOzsO0P3SW9jqPAMz+I+iYtwGnC
+fc3pHOdTRnPcpFLkU2lr/3Im9/SusJAeKO1hnPAV+BPfLcmTvgFahoerYcgXzCCN
+X2ajSZ24wujlGWJryWd3DdJ++rXInGv9vBH3EAxYz8k2DWvoNo2wxFiHvoP9YA+s
+ueP+DoyLvrS/PhwI0geab+XVyBj+uDX0MXA2CGiG3Q9VwpLmawHqIHXt4dfag39i
+xSD3kMAkfnnU1MvWbfoTKXI3cJe+p2cCAwEAAaMmMCQwDgYDVR0PAQH/BAQDAgLk
+MBIGA1UdEwEB/wQIMAYBAf8CAQAwDQYJKoZIhvcNAQELBQADggEBAGeIwU/4OvME
+7T1LZIf4vFj6V4DtsDrZknviEFadGVNAqACcluZlxsWdM7yKqFyrQX4EyzrGbfQH
+hRqinNGMTGk9T0Ps4exN9qETM+TDnL4T+dPawmlIekv1cfrRfZpRYe7hc4Lk37j5
+xy3aMbfsIvT4YsuDXUJRI6UfPjZsQ6QSCNLdb4cSDYee9guSXgEUeO2JDyxODYnd
+2my2Y0Mskh5Y8phnFJRUxCF5VPvnG9UdaAJlrCKNO3rcZjgvs8OWfJv2QxzW/PRq
+5NuztvKRYc7CHdAKl4oJzELrXiQ68tuC/QdfIJrvocObjXgcoBAtbr3wZ/DMmJ2y
+yQCcxjPCUH8=
+-----END CERTIFICATE-----`
+
 func NewConn() (*grpc.ClientConn, error) {
 	opts := []grpc.DialOption{
 		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(100 * 1000 * 1000)),
 	}
 	crt := os.Getenv("DGRAPH_CRT")
 	key := os.Getenv("DGRAPH_KEY")
-	ca := os.Getenv("DGRAPH_CA")
 	if len(crt) > 0 {
 		cert, err := tls.X509KeyPair([]byte(crt), []byte(key))
 		if err != nil {
 			return nil, err
 		}
 		pool := x509.NewCertPool()
-		if !pool.AppendCertsFromPEM([]byte(ca)) {
+		if !pool.AppendCertsFromPEM([]byte(CA)) {
 			return nil, errors.Errorf("invalid CA cert")
 		}
 		opts = append(
--- deployment/upload-secrets.sh
+++ deployment/upload-secrets.sh
@@ -4,7 +4,6 @@ set -x
 
 now secrets rm dgraph-crt
 now secrets rm dgraph-key
-now secrets rm dgraph-ca
 
 set -e
 
@@ -12,4 +11,3 @@ HOST=root@ourgraph-db.fn.lc
 
 now secrets add dgraph-crt -- "$(ssh $HOST cat /data/client.user.crt)"
 now secrets add dgraph-key -- "$(ssh $HOST cat /data/client.user.key)"
-now secrets add dgraph-ca -- "$(ssh $HOST cat /data/ca.crt)"
